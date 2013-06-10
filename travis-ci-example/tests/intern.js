b9f38054d92b7574ed78718ff9328f8da2d33adf
vladikoff <vlad.filippov@gmail.com>
2013-06-10 10:42:29 -0700

    Updating travis example

--- travis-ci-example/tests/intern.js
+++ travis-ci-example/tests/intern.js
@@ -19,7 +19,9 @@ define({
 	// publishing this configuration file somewhere
 	webdriver: {
 		host: 'localhost',
-		port: 4445
+		port: 4445,
+		SAUCE_USERNAME: 'intern-example-ci',
+		SAUCE_ACCESS_KEY: '89ac3089-17b3-4e9b-aaf3-c475b27fa441'
 	},
 
 	// Configuration options for the module loader; any AMD configuration options supported by the Dojo loader can be
