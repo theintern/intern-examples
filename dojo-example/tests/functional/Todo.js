182d2171eccbf3924313d93a50ec852e9f208e20
bitpshr <paulbouchon90@gmail.com>
2013-09-16 15:34:40 -0400

    Remove hardcoded local url in functional test

--- dojo-example/tests/functional/Todo.js
+++ dojo-example/tests/functional/Todo.js
@@ -3,28 +3,25 @@ define([
 	'intern/chai!assert',
 	'require'
 ], function (registerSuite, assert, require) {
-	var request,
-		url = 'http://localhost/Projects/intern-examples/dojo-example';
+	var url = '../../index.html';
 
 	registerSuite({
 		name: 'Todo (functional)',
 
 		'submit form': function () {
 			return this.remote
-				.get('http://localhost/Projects/intern-examples/dojo-example').then(function () {
-					console.log('page loaded');
-				})
-				// .elementById('new-todo')
-				// .clickElement()
-				// .keys('Task 1')
-				// .keys("\n")
-				// .keys('Task 2')
-				// .keys("\n")
-				// .keys('Task 3')
-				// .getAttribute('value')
-				// .then(function (val) {
-				// 	assert.ok(val.indexOf('Task 3') > -1, 'Task 3 should remain in the new todo');
-				// })
+				.get(require.toUrl(url))
+				.elementById('new-todo')
+				.clickElement()
+				.keys('Task 1')
+				.keys('\n')
+				.keys('Task 2')
+				.keys('\n')
+				.keys('Task 3')
+				.getAttribute('value')
+				.then(function (val) {
+					assert.ok(val.indexOf('Task 3') > -1, 'Task 3 should remain in the new todo');
+				});
 		}
 	});
 });
