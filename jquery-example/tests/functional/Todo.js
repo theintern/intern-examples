80e4dddd491b47bf9db3c8da86a0cea8c550914b
Jason Cheatham <j.cheatham@gmail.com>
2017-07-10 09:03:57 -0400

    Update jQuery example for intern4-alpha.6

--- jquery-example/tests/functional/Todo.js
+++ jquery-example/tests/functional/Todo.js
@@ -1,24 +1,21 @@
+/* global intern */
 var registerSuite = intern.getInterface('object').registerSuite;
-var assert = intern.getAssertions('assert');
+var assert = intern.getPlugin('chai').assert;
 
-registerSuite({
-	name: 'Todo (functional)',
-
-	tests: {
-		'submit form': function () {
-			return this.remote
-				.get('./index.html')
-				.findById('new-todo')
-				.click()
-				.pressKeys('Task 1')
-				.pressKeys('\n')
-				.pressKeys('Task 2')
-				.pressKeys('\n')
-				.pressKeys('Task 3')
-				.getSpecAttribute('value')
-				.then(function (val) {
-					assert.ok(val.indexOf('Task 3') > -1, 'Task 3 should remain in the new todo');
-				});
-		}
+registerSuite('Todo (functional)', {
+	'submit form': function () {
+		return this.remote
+			.get('./index.html')
+			.findById('new-todo')
+			.click()
+			.pressKeys('Task 1')
+			.pressKeys('\n')
+			.pressKeys('Task 2')
+			.pressKeys('\n')
+			.pressKeys('Task 3')
+			.getSpecAttribute('value')
+			.then(function (val) {
+				assert.ok(val.indexOf('Task 3') > -1, 'Task 3 should remain in the new todo');
+			});
 	}
 });
